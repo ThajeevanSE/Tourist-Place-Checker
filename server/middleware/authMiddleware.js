4c14ecb47f09d19650680073a5f787866d8eb7ec
Thajeevan Vasanthakumar <thajee@Thajeevans-MacBook-Air.local>
2026-02-26 14:26:48 +0530

    update

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -6,10 +6,10 @@ export const verifyToken = (req, res, next) => {
   if (!token) return res.status(401).json({ message: "Access Denied" });
 
   try {
-    // Token format is usually "Bearer <token>", so we split it
+
     const tokenString = token.split(" ")[1]; 
     const verified = jwt.verify(tokenString, process.env.JWT_SECRET);
-    req.user = verified; // Attach user ID to the request
+    req.user = verified; 
     next(); 
   } catch (error) {
     res.status(400).json({ message: "Invalid Token" });
